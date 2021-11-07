ad6e7899e505280eb44df620f2dff7a66977baef
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2021-11-07 22:12:51 +0100

    Add files via upload

---
+++ public/game/script.js
@@ -0,0 +1,220 @@
+const btn = document.querySelector("#btn");
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scoreDiv = document.querySelector("#score");
+const lenDiv = document.querySelector("#len");
+const endDiv = document.querySelector(".popUp");
+const trybSelect = document.querySelector("#tryb");
+const logoff = document.querySelector("#logoff");
+const bestScores = document.querySelector(".bestScore");
+let choosenSpeed = parseInt(trybSelect.value);
+
+const verifyToken = async () => {
+	const res = await fetch("http://localhost:8080/check", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${sessionStorage.getItem("auth")}`,
+		},
+		body: JSON.stringify({ data: sessionStorage.getItem("user") }),
+	});
+	const response2 = await res.json();
+	if (response2?.error) {
+		window.location.href = `http://localhost:8080/game.html`;
+	} else {
+		displayScore();
+	}
+	return response2;
+};
+
+verifyToken();
+
+document.querySelector(
+	".logout"
+).textContent = `Zalogowany jako: ${sessionStorage.getItem("user")}`;
+
+const checkScores = async () => {
+	const data = { login: sessionStorage.getItem("user") };
+	const res = await fetch("http://localhost:8080/score", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	});
+	const json = await res.json();
+	return json;
+};
+
+const displayScore = async () => {
+	bestScores.textContent = `Ładowanie...`;
+	const scores = await checkScores();
+	const bestScore = Math.max(...scores.score);
+	bestScores.textContent = `Najlepszy wynik: ${bestScore}`;
+};
+
+logoff.addEventListener("click", () => {
+	window.location.href = `http://localhost:8080`;
+	sessionStorage.removeItem("auth");
+	sessionStorage.removeItem("user");
+});
+
+trybSelect.addEventListener("input", () => {
+	choosenSpeed = parseInt(trybSelect.value);
+});
+
+btn.addEventListener("click", () => {
+	const widthInput = document.querySelector("#width");
+	const heightInput = document.querySelector("#height");
+	let points = 0;
+	let start = false;
+	if (!widthInput.value || !heightInput.value) {
+		return alert("Podaj dane!");
+	}
+	canvas.height = heightInput.value;
+	canvas.width = widthInput.value;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	endDiv.style.display = "none";
+	btn.value = "Graj";
+	const sizeContainer = {
+		width: canvas.width,
+		height: canvas.height,
+	};
+	const randomFeed = (min, max) => {
+		return Math.floor((Math.random() * (max - min + 1) + min) / 20) * 20;
+	};
+	const feedPos = {
+		width: 20,
+		height: 20,
+		x: randomFeed(0, sizeContainer.width),
+		y: randomFeed(0, sizeContainer.height),
+	};
+	let dX = 20;
+	let dY = 0;
+	const snakeSegments = [
+		{ x: 20, y: 20 },
+		{ x: 10, y: 20 },
+		{ x: 0, y: 20 },
+	];
+	const drawSnake = () => {
+		snakeSegments.forEach((part) => {
+			ctx.fillRect(part.x, part.y, 20, 20);
+		});
+	};
+	const changeDirection = (event) => {
+		start = true;
+		const LEFT_KEY = 37;
+		const RIGHT_KEY = 39;
+		const UP_KEY = 38;
+		const DOWN_KEY = 40;
+
+		const keyPressed = event.keyCode;
+		const goingUp = dY === -20;
+		const goingDown = dY === 20;
+		const goingRight = dX === 20;
+		const goingLeft = dX === -20;
+		if (keyPressed === LEFT_KEY && !goingRight) {
+			dX = -20;
+			dY = 0;
+		}
+
+		if (keyPressed === UP_KEY && !goingDown) {
+			dX = 0;
+			dY = -20;
+		}
+
+		if (keyPressed === RIGHT_KEY && !goingLeft) {
+			dX = 20;
+			dY = 0;
+		}
+
+		if (keyPressed === DOWN_KEY && !goingUp) {
+			dX = 0;
+			dY = 20;
+		}
+	};
+	const autoMoving = () => {
+		snakeSegments.unshift({
+			x: snakeSegments[0].x + dX,
+			y: snakeSegments[0].y + dY,
+		});
+		if (snakeSegments[0].x === feedPos.x && snakeSegments[0].y === feedPos.y) {
+			createRandomFeed();
+			points += 10;
+		} else {
+			snakeSegments.pop();
+		}
+	};
+	const createRandomFeed = () => {
+		feedPos.x = randomFeed(0, sizeContainer.width);
+		feedPos.y = randomFeed(0, sizeContainer.height);
+		snakeSegments.forEach((part) => {
+			if (part.x === feedPos.x && part.y === feedPos.y) {
+				createRandomFeed();
+			}
+		});
+		if (snakeSegments[0].x === feedPos.x && snakeSegments[0].y === feedPos.y) {
+			ctx.clearRect(feedPos.x, feedPos.x, feedPos.width, feedPos.height);
+			createRandomFeed();
+		}
+	};
+	const creatingFood = () => {
+		if (snakeSegments[0].x === feedPos.x && snakeSegments[0].y === feedPos.y) {
+			ctx.clearRect(feedPos.x, feedPos.x, feedPos.width, feedPos.height);
+			createRandomFeed();
+		}
+	};
+	const displayEndGame = async () => {
+		displayScore();
+		endDiv.style.display = "block";
+		btn.value = "Graj ponownie!";
+		scoreDiv.textContent = `Wynik: ${points}`;
+		lenDiv.textContent = `Długość: ${snakeSegments.length}`;
+		const data = {
+			login: sessionStorage.getItem("user"),
+			score: points,
+			snakeLength: snakeSegments.length,
+		};
+		const res = await fetch("http://localhost:8080/game", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+		console.log(res);
+	};
+	const checkIfCrash = () => {
+		for (let i = 3; i < snakeSegments.length; i++) {
+			if (
+				snakeSegments[i].x === snakeSegments[0].x &&
+				snakeSegments[i].y === snakeSegments[0].y
+			)
+				return true;
+		}
+		return (
+			snakeSegments[0].x < 0 ||
+			snakeSegments[0].x > sizeContainer.width ||
+			snakeSegments[0].y < 0 ||
+			snakeSegments[0].y > sizeContainer.height
+		);
+	};
+	(async (sleep) => {
+		while (true) {
+			if (checkIfCrash()) {
+				return displayEndGame();
+			}
+			document.addEventListener("keydown", changeDirection);
+			console.log(checkIfCrash());
+			if (start) {
+				autoMoving();
+			}
+			creatingFood();
+			ctx.clearRect(0, 0, sizeContainer.width, sizeContainer.height);
+			ctx.strokeRect(0, 0, sizeContainer.width, sizeContainer.height);
+			ctx.fillRect(feedPos.x, feedPos.y, 20, 20);
+			drawSnake();
+			await sleep(choosenSpeed);
+		}
+	})((ms) => new Promise((resolve) => setTimeout(resolve, ms)));
+});
