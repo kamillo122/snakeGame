9146a7ee0c997d7068d851d17a6d6f7ef5230691
kamillo122 <67054069+kamillo122@users.noreply.github.com>
2021-11-08 20:06:33 +0100

    Create getDashboard.js

---
+++ public/getDashboard.js
@@ -0,0 +1,21 @@
+const dashBoard = document.querySelector(".dashboard");
+const table = document.querySelector("table");
+
+const displayDashboard = async () => {
+	const res = await fetch("https://kamilosnakegame.herokuapp.com/dashboard");
+	const dash = await res.json();
+	const sorted = dash.dashboard.sort((a, b) => {
+		const scoreA = a.bestScore;
+		const scoreB = b.bestScore;
+		return scoreA < scoreB ? 1 : scoreA === scoreB ? 0 : -1;
+	});
+	for (const player of sorted) {
+		table.innerHTML += `
+		<tr>
+			<td>${player?.login}</td>
+			<td>${player?.bestScore}</td>
+		</tr>
+		`;
+	}
+};
+displayDashboard();
